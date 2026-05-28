8ace69525f7663ac7e9de600110543c43da0ecec
Muhammad Adam Amir <mathsteacher2003@gmail.com>
2026-05-28 15:46:00 +0500

    removing script

--- app.js
+++ app.js
@@ -1,16 +1,1 @@
 
-function scrollLeftCustom(){
-  document.getElementById('movieRow')
-    .scrollBy({
-      left:-320,
-      behavior:'smooth'
-    });
-}
-
-function scrollRightCustom(){
-  document.getElementById('movieRow')
-    .scrollBy({
-      left:320,
-      behavior:'smooth'
-    });
-}
