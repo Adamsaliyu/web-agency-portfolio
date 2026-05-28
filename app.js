6e00fd483f894b030db4fd4b4d4c8354b1f0fded
Muhammad Adam Amir <mathsteacher2003@gmail.com>
2026-05-28 15:47:29 +0500

    adding script note

--- app.js
+++ app.js
@@ -1,1 +1,56 @@
 
+const track = document.querySelector(".slider-track");
+const slides = document.querySelectorAll(".slide-card");
+
+const nextBtn = document.getElementById("next");
+const prevBtn = document.getElementById("prev");
+
+let currentSlide = 0;
+const totalSlides = slides.length;
+
+/* UPDATE SLIDER */
+function updateSlider(){
+
+  track.style.transform =
+  `translateX(-${currentSlide * 100}%)`;
+
+}
+
+/* NEXT BUTTON */
+nextBtn.addEventListener("click", () => {
+
+  currentSlide++;
+
+  if(currentSlide >= totalSlides){
+    currentSlide = 0;
+  }
+
+  updateSlider();
+
+});
+
+/* PREV BUTTON */
+prevBtn.addEventListener("click", () => {
+
+  currentSlide--;
+
+  if(currentSlide < 0){
+    currentSlide = totalSlides - 1;
+  }
+
+  updateSlider();
+
+});
+
+/* AUTO SLIDE */
+setInterval(() => {
+
+  currentSlide++;
+
+  if(currentSlide >= totalSlides){
+    currentSlide = 0;
+  }
+
+  updateSlider();
+
+}, 4000);
